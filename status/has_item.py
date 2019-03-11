9cf596b4617af64a2ec03ca834e9de44747d4cc0
TomokiPC-Parent\tomokis <ts32worker@gmail.com>
2019-03-12 01:15:06 +0900

    所持アイテムのステート切り替えで一定時間スルーする処理を追加

--- status/has_item.py
+++ status/has_item.py
@@ -5,7 +5,13 @@
 from bottle import get, post, request
 import sqlite3
 import configparser
+from datetime import datetime as dt
 
+
+# 定数定義
+DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S"
+WAITSEC_AFTER_PUT = 5.0
+INVALID_ID = 2
 
 # 設定ファイル読み込み
 config = configparser.ConfigParser()
@@ -25,8 +31,11 @@ def reset():
 	"""
 	cursor = conn.cursor()
 	cursor.execute("DROP TABLE IF EXISTS %s" % DBTABLE)
-	cursor.execute("CREATE TABLE IF NOT EXISTS %s (id INTEGER PRIMARY KEY, name TEXT, value int)" % DBTABLE)
-	cursor.execute("INSERT INTO %s VALUES (?, ?, ?)" % DBTABLE, (DEFAULTID, DEFAULTNAME, 0))
+	cursor.execute("CREATE TABLE IF NOT EXISTS %s (id INTEGER PRIMARY KEY, name TEXT, value int, last_put_time TEXT)" % DBTABLE)
+	cursor.execute(
+		"INSERT INTO %s VALUES (?, ?, ?, ?)" % DBTABLE,
+		(DEFAULTID, DEFAULTNAME, 0, dt.now().strftime(DATETIME_FORMAT))
+	)
 	conn.commit()
 	return {}
 
@@ -36,7 +45,10 @@ def get():
 	"""所持フラグを立てます。
 	"""
 	cursor = conn.cursor()
-	cursor.execute("UPDATE %s SET value = ? WHERE name = ?" % DBTABLE, (1, DEFAULTNAME))
+	cursor.execute(
+		"UPDATE %s SET value = ? WHERE name = ?" % DBTABLE,
+		(1, DEFAULTNAME)
+	)
 	conn.commit()
 	return {}
 
@@ -46,7 +58,10 @@ def put():
 	"""所持フラグを解除します。
 	"""
 	cursor = conn.cursor()
-	cursor.execute("UPDATE %s SET value = ? WHERE name = ?" % DBTABLE, (0, DEFAULTNAME))
+	cursor.execute(
+		"UPDATE %s SET value = ?, last_put_time = ? WHERE name = ?" % DBTABLE,
+		(0, dt.now().strftime(DATETIME_FORMAT), DEFAULTNAME)
+	)
 	conn.commit()
 	return {}
 
@@ -56,13 +71,21 @@ def check():
 	"""所持したかどうかを確認します。
 	"""
 	cursor = conn.cursor()
-	cursor.execute("SELECT value FROM %s WHERE name = ?" % DBTABLE, (DEFAULTNAME,))
+	cursor.execute(
+		"SELECT value, strftime(last_put_time) FROM %s WHERE name = ?" % DBTABLE,
+		(DEFAULTNAME,)
+	)
 	result = cursor.fetchone()
+
 	if result is None:
 		return {
 			"result": -1,
 		}
 	else:
+		if result[0] == 1 and (dt.now() - dt.strptime(result[1], DATETIME_FORMAT)).total_seconds() < WAITSEC_AFTER_PUT:
+			# 所持していない場合、最後に置いたときから一定時間経っていないと無効にする: アナログGPIO入力が過敏に反応するのを回避するため
+			result = [INVALID_ID]
+
 		return {
 			"result": result[0],
 		}
