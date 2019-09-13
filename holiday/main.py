67c071e39ea023734cf72c15f28ccd19bdd9edc7
TomokiPC-Parent\tomokis <ts32worker@gmail.com>
2019-09-14 00:03:19 +0900

    休日判定: GCF用に自由型API追加

--- holiday/main.py
+++ holiday/main.py
@@ -1,11 +1,14 @@
 ##########################################################################################
 #    祝日を含む休日を判定するAPIです。
 ##########################################################################################
+from datetime import datetime
 from datetime import date
 import jpholiday
+import json
 
 
 # 定数定義
+DATE_FORMAT = "%Y/%m/%d"
 WEEKDAYS = {
 	"月": 0,
 	"火": 1,
@@ -31,6 +34,38 @@ def _is_holiday(dt):
 		return True
 	else:
 		return False
+
+
+def is_holiday(request):
+	"""指定された日付が休日であるかどうかを返します。
+
+	Arguments:
+		request (flask.Request): HTTP request object.
+	Returns:
+		{bool} -- 土日祝のいずれかに当てはまる場合はTrueを返します。
+	"""
+	if request.method != "POST":
+		return ("", 405, None)
+
+	request_json = request.get_json()
+	if not "date" in request_json:
+		return ("date要素がありません", 400, None)
+
+	try:
+		dt = datetime.strptime(request_json["date"], DATE_FORMAT)
+		dt = date(dt.year, dt.month, dt.day)
+	except:
+		return ("日付は yyyy/mm/dd の形式の文字列にして下さい", 400, None)
+
+	result = {}
+	if dt.weekday() in [WEEKDAYS["土"], WEEKDAYS["日"]]:
+		result["is_holiday"] = True
+	elif jpholiday.is_holiday(dt):
+		result["is_holiday"] = True
+	else:
+		result["is_holiday"] = False
+
+	return (json.dumps(result), 200, None)
 
 
 @post("/holiday/check")
