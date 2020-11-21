947d9876c88a0cbce030bac498cf275916132e80
TomokiPC-Parent\tomokis <ts32worker@gmail.com>
2020-11-21 19:47:10 +0900

    [weather] Docker対応、単体テスト導入

---
+++ weather/server.py
@@ -0,0 +1,19 @@
+##########################################################################################
+#    内蔵アプリケーションサーバーを起動します。
+##########################################################################################
+from flask import Flask
+import api.main as main
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    return main.check()
+
+
+if __name__ == "__main__":
+    app.run(
+        debug=False,
+        host="0.0.0.0",
+        port=8080
+    )
